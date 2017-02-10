0a01b4c68b3006ca1c02b4b5381c26037d8e3c7e
kazukikimura <kazukacchin@gmail.com>
2017-02-10 06:32:03 +0000

    Gemfile update for heroku

--- db/migrate/20170210054551_add_age_to_messages.rb
+++ db/migrate/20170210054551_add_age_to_messages.rb
@@ -1,6 +1,5 @@
 class AddAgeToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :age, :integer
-    validates:age, numericality:{only_integer:true, minimum:0, maximum:150}
   end
 end
